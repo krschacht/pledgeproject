9adc62b964dac202e8c0136c2565ea798d281fff
Keith Schacht <krschacht@gmail.com>
2010-05-28 18:57:54 -0500

    format and add countdown timers

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready( function() {
+
+  $('.has_countdown').each( function() {
+    $(this).countdown( {
+      until: new Date( $(this).attr( 'until' ) ),
+      compact: true,
+      timezone: -6,
+      format: 'dHMS',
+      description: ''
+    } );
+  } );
+
+} );
