2e9b36534d42d3b7014f1fd4784d26e73af20d91
Keith Schacht <krschacht@gmail.com>
2010-06-16 17:30:36 -0500

    improve the overall layout and add master css scoping

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProjectsController < ApplicationController
 
-  before_filter :require_admin
+  before_filter :require_admin, :show_admin_nav
   
   # GET /projects
   # GET /projects.xml
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
 
-  helper_method :current_user_admin?
+  helper_method :current_user_admin?, :action_name_safe
 
 
   def current_user_admin?
@@ -13,4 +13,13 @@ class ApplicationController < ActionController::Base
     redirect_to "/"  unless current_user_admin?
   end
 
+  def show_admin_nav
+    @show_admin_nav = true
+  end
+  
+  def action_name_safe
+    # Eliminate any invalid iv characters from action.
+    params[:action].gsub( /\W/, '' )
+  end
+  
 end
