ec7275f335375f976ffdea16da16265fbc0b5d63
Keith Schacht <krschacht@gmail.com>
2010-06-21 18:19:37 -0500

    remove hoptoad

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,8 @@ module Pledgeproject
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    # config.gem 'logworm_client'
+        
   end
 end
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+# HoptoadNotifier.configure do |config|
+#   config.api_key = '354cbe3027f7cd95d23004fa62856e42'
+# end
