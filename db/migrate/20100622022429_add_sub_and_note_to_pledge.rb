12aae7a5d8afc545d0157111244400ad42ff5853
Keith Schacht <krschacht@gmail.com>
2010-06-21 21:47:15 -0500

    change migration default datatype for postgres

--- db/migrate/20100622022429_add_sub_and_note_to_pledge.rb
+++ db/migrate/20100622022429_add_sub_and_note_to_pledge.rb
@@ -1,6 +1,6 @@
 class AddSubAndNoteToPledge < ActiveRecord::Migration
   def self.up
-    add_column :pledges, :subscribe_me, :boolean, :default => 1
+    add_column :pledges, :subscribe_me, :boolean, :default => true
     add_column :pledges, :internal_note, :string
   end
 
