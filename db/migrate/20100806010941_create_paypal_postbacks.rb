0a91252bf473f3fae1506506d6e41e0a21fc1486
Keith Schacht <krschacht@gmail.com>
2010-09-16 22:12:35 -0700

    fix migration

--- db/migrate/20100806010941_create_paypal_postbacks.rb
+++ db/migrate/20100806010941_create_paypal_postbacks.rb
@@ -1,7 +1,5 @@
 class CreatePaypalPostbacks < ActiveRecord::Migration
   def self.up
-    drop_table :paypal_postbacks
-
     create_table :paypal_postbacks do |t|
       t.integer :premium_transaction_id
       t.string :payer_business_name
